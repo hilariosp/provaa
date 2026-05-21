ac1511ee0c9d2a781039806b1391bf2bb1e8065c
Hilário <145464560+hilariosp@users.noreply.github.com>
2026-05-20 21:37:05 -0300

    Add files via upload

--- Avaliacao.java
+++ Avaliacao.java
@@ -1,7 +1,36 @@
+void main (){
 
-/* Se você está vendo esta mensagem é porque fez tudo certo até aqui. 
-    Boa prova! */
+    int pecasporlote=0;
+    int pecas=0;
+    int lotes=0;
 
-void main() {
-    IO.println("Boa Prova!");
+
+    while (true){
+
+        pecasporlote = Integer.parseInt(IO.readln("Digite quantas peças defeituosas encontrou nesse lote: "));
+        if (pecasporlote>=0){
+            pecas += pecasporlote;
+            lotes++;
+        }
+        if (pecasporlote<0){
+            if (lotes==0)
+                IO.println("Não use números negativos!");
+            else
+                break;
+        }
+
+    }
+
+    float mediaobtida = (float) pecas/lotes;
+    IO.println(pecas + " peça(s) defeituosa(s).");
+    IO.println(lotes + " lote(s) analisado(s).");
+    IO.println(mediaobtida + " = média obtida.");
+
+    if (mediaobtida<=2)
+        IO.println("Máquina Regulada (Excelente)");
+    if (mediaobtida>2 && mediaobtida<=5)
+        IO.println("Alerta: Necessita de Manutenção Preventiva");
+    if (mediaobtida>5)
+        IO.println("Paragem Crítica: Máquina Danificada");
+
 }
